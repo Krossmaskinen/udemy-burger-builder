7055f93c32c130b2577d2da9b377671019eea0f6
Bill <bill.kurtson@gmail.com>
2018-09-11 13:59:24 +0200

    Feat/sidedrawer (#14)
    * - add SideDrawer component
    - render SideDrawer in Layout
    
    * - set Logo height in components using the Logo, instead of in the Logo component
    - adjust Logo height in Toolbar
    - adjust Logo height in SideDrawer
    
    * - style SideDrawer
    - update styling for NavigationItems and navigationItem
    - hide toolbar NavigationItems on smaller screens
    
    * - create DrawerToggle component
    - show and hide SideDrawer dynamically
    
    * - update DrawerToggle styling
    - bugfix: Fix issue where Modal covered up some space in the application even when it was not in view

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import classes from './SideDrawer.css';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import ReactAux from '../../../hoc/ReactAux';
+
+const sideDrawer = (props) => {
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
+
+    attachedClasses = attachedClasses.join(' ');
+
+    return (
+        <ReactAux>
+            <Backdrop 
+                show={props.open}
+                clicked={props.closed}
+            />
+            <div className={attachedClasses}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
+            </div>
+        </ReactAux>
+    );
+};
+
+export default sideDrawer;
