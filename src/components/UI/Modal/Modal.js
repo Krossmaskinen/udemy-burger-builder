f1101d9928d50eb91f6ca8b018afe43114c9f13f
Bill <bill.kurtson@gmail.com>
2018-09-12 10:13:02 +0200

    Release 0.2 (#20)
    * Feat/checkout button (#10)
    
    * - add order button to BuilderControls
    
    * - add styling for order now button
    - disable order now button if no ingredients are chosen
    
    * - add Modal
    - add Backdrop
    - show order summary in Modal
    
    * - show Modal when "order now" button is clicked
    
    * - add Backdrop
    - show Backdrop when modal is open
    - close Modal when Backdrop is clicked
    
    * - Add Cancel and Continue buttons to OrderSummary
    - add Button component
    
    * - add Toolbar component (#11)
    
    - add Toolbar to Layout
    
    * - add and display Logo in Toolbar (#12)
    
    * - add NavigationItems component (#13)
    
    - add NavigationItem component
    - rendor NavigationItems in Toolbar
    
    * Feat/sidedrawer (#14)
    
    * - add SideDrawer component
    - render SideDrawer in Layout
    
    * - set Logo height in components using the Logo, instead of in the Logo component
    - adjust Logo height in Toolbar
    - adjust Logo height in SideDrawer
    
    * - style SideDrawer
    - update styling for NavigationItems and navigationItem
    - hide toolbar NavigationItems on smaller screens
    
    * - create DrawerToggle component
    - show and hide SideDrawer dynamically
    
    * - update DrawerToggle styling
    - bugfix: Fix issue where Modal covered up some space in the application even when it was not in view
    
    * Bugfix sidedrawer showing default (#15)
    
    * - add SideDrawer component
    - render SideDrawer in Layout
    
    * - set Logo height in components using the Logo, instead of in the Logo component
    - adjust Logo height in Toolbar
    - adjust Logo height in SideDrawer
    
    * - style SideDrawer
    - update styling for NavigationItems and navigationItem
    - hide toolbar NavigationItems on smaller screens
    
    * - create DrawerToggle component
    - show and hide SideDrawer dynamically
    
    * - update DrawerToggle styling
    - bugfix: Fix issue where Modal covered up some space in the application even when it was not in view
    
    * - bugfix: SideDrawer showing default
    
    * feat/sidedrawer (#16)
    
    * - add NavigationItems component (#13)
    
    - add NavigationItem component
    - rendor NavigationItems in Toolbar
    
    * Feat/sidedrawer (#14)
    
    * - add SideDrawer component
    - render SideDrawer in Layout
    
    * - set Logo height in components using the Logo, instead of in the Logo component
    - adjust Logo height in Toolbar
    - adjust Logo height in SideDrawer
    
    * - style SideDrawer
    - update styling for NavigationItems and navigationItem
    - hide toolbar NavigationItems on smaller screens
    
    * - create DrawerToggle component
    - show and hide SideDrawer dynamically
    
    * - update DrawerToggle styling
    - bugfix: Fix issue where Modal covered up some space in the application even when it was not in view
    
    * Bugfix sidedrawer showing default (#15)
    
    * - add SideDrawer component
    - render SideDrawer in Layout
    
    * - set Logo height in components using the Logo, instead of in the Logo component
    - adjust Logo height in Toolbar
    - adjust Logo height in SideDrawer
    
    * - style SideDrawer
    - update styling for NavigationItems and navigationItem
    - hide toolbar NavigationItems on smaller screens
    
    * - create DrawerToggle component
    - show and hide SideDrawer dynamically
    
    * - update DrawerToggle styling
    - bugfix: Fix issue where Modal covered up some space in the application even when it was not in view
    
    * - bugfix: SideDrawer showing default
    
    * - only update Modal component if it's showing (#17)
    
    * - update folder structure (#18)

---
+++ src/components/UI/Modal/Modal.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import classes from './Modal.css';
+import ReactAux from '../../../hoc/ReactAux/ReactAux';
+import Backdrop from '../Backdrop/Backdrop';
+
+class Modal extends Component {
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show;
+    }
+
+    render() {
+        return (
+            <ReactAux>
+                <Backdrop
+                    show={this.props.show}
+                    clicked={this.props.modalClosed}
+                />
+                <div className={classes.Modal}
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-200vh)',
+                        opacity: this.props.show ? '1' : '0'
+                    }}
+                >
+                    {this.props.children}
+                </div>
+            </ReactAux>
+        );
+    }
+}
+
+export default Modal;
