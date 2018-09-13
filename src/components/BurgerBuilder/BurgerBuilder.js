6986680c4022dd98a0632f9af07100eb358e9fd6
Bill <bill.kurtson@gmail.com>
2018-09-13 16:07:17 +0200

    - add withErrorHandler hoc - add error handling to BurgerBuilder

--- src/components/BurgerBuilder/BurgerBuilder.js
+++ src/components/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,8 @@ import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import axios from '../../axios-orders';
 import Spinner from '../UI/Spinner/Spinner';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
+import axiosInstance from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -153,4 +155,5 @@ class BurgerBuilder extends Component {
     }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axiosInstance);
+// export default BurgerBuilder;
