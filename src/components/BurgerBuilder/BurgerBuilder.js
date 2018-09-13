95572328e52893a8cc86ae049c68e6f6d4c60bde
Bill Kurtson <bill.kurtson@gmail.com>
2018-09-13 12:44:31 +0200

    - bugfix: typo in axios-orders broke the baseURL - post burger order to DB when ordering

--- src/components/BurgerBuilder/BurgerBuilder.js
+++ src/components/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../Burger/Burger';
 import BuildControls from '../Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -49,7 +50,30 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You continue!');
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Bill Kurtson',
+                address: {
+                    street: 'street 1',
+                    zipCode: '12435',
+                    country: 'Sweden'
+                },
+                email: 'test@test.com'
+            },
+            devileryMethod: 'fastest'
+        };
+
+        axios.post('orders.json', order)
+            .then(response => {
+                console.log(response);
+            })
+            .catch(error => {
+                console.error(error);
+
+                return Promise.reject(error);
+            });
     }
 
     addIngredientHandler = (type) => {
